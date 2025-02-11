4e36e62bac0b4b379f6e35aa0b085a23f8ee6773
Hasib <mdhasib0296@gmail.com>
2025-02-11 23:12:07 +0600

    new commit

--- my_homework/class6.py
+++ my_homework/class6.py
@@ -1,4 +1,4 @@
-#dictionary
+# dictionary
 ''' for example in oxford dictionary there are some words and then the meaning of those words. but a word can have multiple meaning . in python the dictionary works exactly the same. there are two main things . one is key whcih stands for the word and the value which stands for meaning . just like dictionary a key can have multiple values but not vice versa. 
 '''
 
@@ -23,3 +23,34 @@ print(dict2[True])
 print(dict2.keys())
 print(dict2.values())
 '''
+
+'''
+classroom = {
+    'girls': {'a':11, 'b':(22,44), 'c':33, 'd':{'roll':(55,66)}},
+    'boys': ['d', 'e', 'f']
+}
+
+print(classroom['girls'])
+print(classroom['girls']['b'])
+print(type(classroom['girls']['b']))
+print(classroom['girls']['b'][0])
+print(classroom['girls']['d'].keys())
+print(classroom['girls']['d'].values())
+
+classroom['girls']['e'] = 100
+classroom['girls']['a'] = 111
+del classroom['girls']['c']
+print(classroom['girls'])
+'''
+
+classroom_girls = {'girls': {'a':11, 'b':(22,44), 'c':33, 'd':{'roll':(55,66)}}}
+classroom_boys = {'boys': ['d', 'e', 'f']}
+classroom_girls.update(classroom_boys)
+print(classroom_girls['girls'])
+print(classroom_girls.get('girls'))
+print(classroom_girls.get('girl'))
+print(classroom_girls.get('girl', 50))
+
+
+list1 = [10,20,30,40]
+print(list1.index(20))
