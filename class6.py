da5b951faf346ba2906533036cf9dae8cd5c3121
Hasib <mdhasib0296@gmail.com>
2025-02-11 21:32:28 +0600

    first commit

---
+++ class6.py
@@ -0,0 +1,6 @@
+#dictionary
+''' for example in oxford dictionary there are some words and then the meaning of those words. but a word can have multiple meaning . in python the dictionary works exactly the same. there are two main things . one is key whcih stands for the word and the value which stands for meaning . just like dictionary a key can have multiple values but not vice versa. 
+'''
+
+dict1 = {'key': 'value'}
+print(dict1['key'])
