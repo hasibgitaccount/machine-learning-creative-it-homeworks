0af6a7732d532a6d11d0f9850a8c5ab6f43cec48
Hasib <mdhasib0296@gmail.com>
2025-02-14 15:23:26 +0600

    did some coding

---
+++ class2_20@january.py
@@ -0,0 +1,25 @@
+print('hello world')
+
+# an operator in python is a symbol that performs an operation on values and variables.
+'''
+#1. ARITHMETIC OPERATOR:
+
+#a. additon.
+print(2+2) = 4 # here 2 is operand and the (+) symbol is the operator.
+#b. substraction.
+print(3-2) = 1
+#c. multiply.
+3*5 = 15
+#d. division.
+print(6/3) = 2
+#e.floor division.
+print(10 / 3) = 3 # will get the answer without any decimal.
+#exponentiation
+print(3**4) = 81
+#modulus
+print(15%2) = 1 # will get only the decimal part but not in literal way but after the division, the amount that you cannot do division with as whole integer form.
+'''
+
+# 2. COMPARISON OPERATOR.
+
+#
