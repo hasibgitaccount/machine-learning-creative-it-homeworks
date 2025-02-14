6f1ede1b5325e3ca32ae95d9c69055fdbab7e572
Hasib <mdhasib0296@gmail.com>
2025-02-14 19:02:48 +0600

    did some coding

--- class2_20@january.py
+++ class2_20@january.py
@@ -1,4 +1,7 @@
+'''
 print('hello world')
+'''
+
 
 # an operator in python is a symbol that performs an operation on values and variables.
 '''
@@ -20,6 +23,34 @@ print(3**4) = 81
 print(15%2) = 1 # will get only the decimal part but not in literal way but after the division, the amount that you cannot do division with as whole integer form.
 '''
 
+'''
 # 2. COMPARISON OPERATOR.
 
-#
+# a. equal to.
+print(2 == 3) 
+# b. not equal to .
+print(2 != 3)
+# c. less than
+print(2 < 3)
+# d. greater than 
+print(2 > 3)
+# e. greater than or equal to
+print(2 >= 3)
+# f. less than or equal to
+print(2 <= 3)
+'''
+
+'''
+#variable: variable is a temporary container where we can store values.
+my_first_variable = 2 # here we are storing values inside this container called variable.
+print(my_first_variable) # we can check it using print function.
+
+# how to name a variable
+# 1. the first character has to be small or capital letter. - A, b
+# 2. it cannot have number or special as the first lettr but after the first letter(small letter) - %$&*
+# 3. first letter can have a underscore (_). - _my_variable
+# 4. python reserved keywords, always try to use them as variable names.
+# 5. case sensitive. python recognizes small and capital letter differently. try to keep that in mind. a, A
+'''
+
+
