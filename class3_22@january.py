bb4d172e14d1ed96af921c51b38a4331fa515828
Hasib <mdhasib0296@gmail.com>
2025-02-14 20:48:15 +0600

    first bit of code

--- class3_22@january.py
+++ class3_22@january.py
@@ -1,8 +1,16 @@
 # DATA TYPES
  
 '''
-1.string
-2.integer
-3. float
-4. boolean
+1.string. string data type is any data wrapped around with quotation marks. whether it is (' or ")
+2.integer. integer data type is any numeric data without decimal numbers
+3. float. float data type is any numeric data with decimal numbers.
+4. boolean. it only has two type. 1. True, 2. False
 ''' 
+
+'''
+int_data_type = (10, 5.7, 'pretty_girl', True)
+print(type(int_data_type[0]))# integer
+print(type(int_data_type[1]))# float
+print(type(int_data_type[2]))# string
+print(type(int_data_type[3]))# boolean
+'''
