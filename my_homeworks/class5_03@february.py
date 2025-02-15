dc3baa669537a504f6cfc8171eb2acdcf0f51353
Hasib <mdhasib0296@gmail.com>
2025-02-15 17:07:14 +0600

    i forgot about this one

--- my_homeworks/class5_03@february.py
+++ my_homeworks/class5_03@february.py
@@ -36,3 +36,5 @@ print(list2[1])
 list4 = [10,30,20,40,50,60,70,80,90]
 print(list4.index(30)) # this is index lookup. with it you can know the index of any element inside the list.
 print(list4[1]) # this is classic indexing.
+
+
