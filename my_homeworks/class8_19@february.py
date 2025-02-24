9137deeceac3eb5718b54c56dca3c611a3c46a86
Hasib <mdhasib0296@gmail.com>
2025-02-24 17:44:12 +0600

    started coding

---
+++ my_homeworks/class8_19@february.py
@@ -0,0 +1,8 @@
+# LOOPS
+# to reduce the repetition. 
+# instead of modifying every element in a data structure manually, loops can do that in one line.
+loop1 = [3,4,5,6,7,8]
+
+for i in loop1:
+    '''print(i, i**2)'''
+
