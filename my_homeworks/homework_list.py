9f1a4805d5045f2ba1806990c58e49fd0845b7a9
Hasib <mdhasib0296@gmail.com>
2025-02-27 21:58:57 +0600

    unfinished, stuck in no.2

--- my_homeworks/homework_list.py
+++ my_homeworks/homework_list.py
@@ -1,3 +1,14 @@
 # 1. Write a Python program to sum all the items in a list.
-'''list1 = [1,2,3,4]
-print(sum(list1))'''
+def calculate_sum():
+    list1 = [1,2,3,4]
+    return sum(list1)
+cal_sum = calculate_sum()
+print(cal_sum)
+
+# 2. Write a Python program to multiplies all the items in a list.
+'''def multiply():
+    list2 = [1,2,3,4]
+    for i in list2:
+        return (i**2)
+mul = multiply()
+print(mul)'''
