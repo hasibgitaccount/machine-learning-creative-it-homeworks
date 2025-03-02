c234f8e28538e560919866aa34607cfc97be97ec
Hasib <mdhasib0296@gmail.com>
2025-03-02 22:34:58 +0600

    finished 5 no. answer

--- my_homeworks/homework_list.py
+++ my_homeworks/homework_list.py
@@ -50,3 +50,16 @@ print(sma)
 
 # 5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
 
+def string(str):
+    result = 0
+    for i in str:
+        if len(i) > 1 and i[0] == i[-1]:
+            result += 1
+        else:
+            pass
+    return result
+
+list5 = ['abc', 'aba', '1221']
+count = string(list5)
+print(count)
+
