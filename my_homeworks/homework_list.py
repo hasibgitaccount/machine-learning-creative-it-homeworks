37f1e9e7018696603959bb7d91ad929f996270e5
Hasib <mdhasib0296@gmail.com>
2025-03-02 17:19:16 +0600

    modified 1st and 2nd answer

--- my_homeworks/homework_list.py
+++ my_homeworks/homework_list.py
@@ -1,14 +1,22 @@
 # 1. Write a Python program to sum all the items in a list.
-def calculate_sum():
-    list1 = [1,2,3,4]
-    return sum(list1)
-cal_sum = calculate_sum()
+def calculate_sum(s):
+    result = 0
+    for i in s:
+        result = result + i
+    return result
+
+list1 = [1,2,3,4]
+cal_sum = calculate_sum(list1)
 print(cal_sum)
 
 # 2. Write a Python program to multiplies all the items in a list.
-'''def multiply():
-    list2 = [1,2,3,4]
-    for i in list2:
-        return (i**2)
-mul = multiply()
-print(mul)'''
+def multiply(mu):
+    result = 1
+    for i in mu:
+        '''result = result * i'''
+        result *= i
+    return result
+
+list2 = [1,2,3,4]
+mul = multiply(list2)
+print(mul)
