294e982b7819f1b73674c60c4888d93835e4e231
Hasib <mdhasib0296@gmail.com>
2025-03-02 22:59:48 +0600

    finished question 6

--- my_homeworks/homework_list.py
+++ my_homeworks/homework_list.py
@@ -63,3 +63,13 @@ list5 = ['abc', 'aba', '1221']
 count = string(list5)
 print(count)
 
+# 6. Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
+def tuple_sort(tuple):
+    return sorted(tuple, key=lambda x: x[-1])
+
+sort = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+sorty = tuple_sort(sort)
+print(sorty)
+
+
+
